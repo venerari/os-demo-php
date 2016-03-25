65b1212ecb5f54d2b2063cab81fc5f5bb9489756
Boris Baryshnikov <BorisB@BorisB-Mac.home>
2016-03-25 03:00:04 -0700

    Initial test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # os-demo-php
+
+Demo app to be used with OpenShift Enterprise cluster on Microsoft Azure. Originally created for Microsoft Mechanics show.
+
+Based on https://github.com/rlucente-se-jboss/phpmysqldemo
---
+++ index.php
@@ -0,0 +1,69 @@
+<html>
+ <head>
+  <title>OpenShift: Hello Microsoft Mechanics!</title>
+  <style>
+    table, th, td {
+      border: 1px solid black;
+      border-collapse: collapse;
+    } 
+    th, td {
+      padding: 5px;
+      text-align: left;
+    }
+  </style>
+ </head>
+ <body>
+ <h1>OpenShift: Hello Microsoft Mechanics!</h1>
+<?php
+error_reporting(E_ERROR);
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$port = getenv("MYSQL_SERVICE_PORT");
+$database = getenv("MYSQL_SERVICE_DATABASE");
+$username = getenv("MYSQL_SERVICE_USERNAME");
+$password = getenv("MYSQL_SERVICE_PASSWORD");
+
+$conn = mysqli_connect($host, $username, $password, $database, $port);
+if ($conn) {
+  echo "Database is available <br/>";
+  $sql = "CREATE TABLE visitors (
+          id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+          containerip VARCHAR(15) NOT NULL,
+          visitstamp VARCHAR(30) NOT NULL)";
+  if (mysqli_query($conn, $sql)) {
+    echo "Table visitors created successfully <br/>";
+  } else {
+    echo mysqli_error($conn) . "<br/>";
+  }
+
+  $containerip = $_SERVER['SERVER_ADDR'];
+  $visitstamp = date("D M j G:i:s T Y");
+  $sql = "INSERT INTO visitors (containerip, visitstamp)
+          VALUES ('$containerip', '$visitstamp')";
+
+  if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully <br/>";
+  } else {
+    echo "Error: " . $sql . " - " . mysqli_error($conn) . "<br/>";
+  }
+
+  echo "<h3> Visitor Log </h3>";
+  $sql = "SELECT id, containerip, visitstamp FROM visitors";
+  $result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0) {
+    echo "<table><tr><th>Id</th><th>Container</th><th>Timestamp</th></tr>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr><td>" . $row["id"] . "</td><td>" . $row["containerip"] . "</td><td>" . $row["visitstamp"] . "</td></tr>";
+    }
+    echo "</table>";
+  } else {
+    echo "0 results";
+  }
+
+  mysqli_close($conn);
+} else {
+  echo "Database is not available";
+}
+?>
+ </body>
+</html>
